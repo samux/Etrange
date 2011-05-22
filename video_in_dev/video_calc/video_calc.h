6198a2126ffb6e3278f26c37024eddb3bb701760
Caroline Keramsi <caroline.keramsi@gmail.com>
2011-05-22 23:34:37 +0200

    Video_calc written but not used in top.cpp and never compiled

--- video_in_dev/video_calc/video_calc.h
+++ video_in_dev/video_calc/video_calc.h
@@ -5,10 +5,11 @@
 #include "wb_master_module.h"
 #include "../segmentation.h"
 #define NBPACK 32
-#define T_W  16, //largeur de la tuile traitée
-#define T_H  16,  //hauteur de la tuile traitée
-#define C_W  16, //largeur de la zone en cache,
-#define C_H  16, //hauteur de la zone en cache
+#define T_W  16 //largeur de la tuile traitée
+#define T_H  16  //hauteur de la tuile traitée
+#define C_W  64 //largeur de la zone en cache,
+#define C_H  64 //hauteur de la zone en cache
+#define F_SIZE 3 //Taille de la fifo en nombre de tuile
 
 using namespace sc_core;
 using namespace std;
@@ -32,29 +33,50 @@ namespace soclib { namespace caba {
 			sc_core::sc_in<bool> p_resetn;
 			soclib::caba::WbMaster<wb_param> p_wb;
 
+			//Interruptions de fin de lecture
+			//et de fin d'écriture de l'image
+			sc_out<bool> img_r;
+			sc_out<bool> img_w;
+
 			/////////////////////////////////////
 			// Constructeur
 			/////////////////////////////////////
 
 			VideoCalc (sc_module_name insname,
 					uint32_t * tab,
+					const int w = 640,		//largeur d'image par défaut
+					const int h = 480,		//hauteur d'image par défaut
 					);
 
 			//////////////////////////////////////
 			// Methodes et parametres internes
 			/////////////////////////////////////
-			void read_tule();
-			void store_tule();
+			void get_cache();
+			void process_tile();
+			void store_tile();
 
 		private:
 
 			//Pour contenir la zone en cache
-			unsigned char cache[T_H][T_W];
-			
+			unsigned char cache[C_H][C_W];
+
+			//Coordonnées du pixel au centre de la zone de cache
+			int cache_center_c;
+			int cache_center_l;
+
+			//paramètres de l'image
+			const uint32_t p_WIDTH ;
+			const uint32_t p_HEIGHT ;
+
+			//Pour indiquer qu'il est temps de remplir
+			//le cache
+			sc_signal<bool> ask_cache;
+
+			//Pour indique que le cache a été rempli
+			sc_signal<bool> cache_rdy;
+
 			//Fifo de tuile à stocker en RAM
 			sc_fifo<unsigned char> fifo;
-
-
 
 			//Adresse de lecture et ecriture en RAM
 			uint32_t * wb_tab;
