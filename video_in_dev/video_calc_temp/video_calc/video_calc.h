75746615d94cfd9720f7fe4a5f7f8d95968268d1
Florian Thorey <florian.thorey@gmail.com>
2011-06-14 10:08:21 +0200

    Ajouter ancienne version VCALC dans video_calc_temp.

---
+++ video_in_dev/video_calc_temp/video_calc/video_calc.h
@@ -0,0 +1,125 @@
+#ifndef VIDEO_CALC_H
+# define VIDEO_CALC_H
+
+#include <systemc>
+#include "wb_master_module.h"
+#include "../segmentation.h"
+
+// Largeur tuile traitée
+# define T_W 16
+// Hauteur tuile traitée
+# define T_H 16
+// Largeur zone buffer
+# define B_W 32
+// Hauteur zone buffer
+# define B_H 32
+// Taille FIFO en nb de tuiles
+# define F_SIZE 4
+// Nb tuiles d'entrée dans le buffer
+# define T_IN_NB (p_WIDTH * p_HEIGHT) / (B_W * B_H)
+// Nb tuiles de sortie
+# define T_OUT_NB (p_WIDTH * p_HEIGHT) / (T_W * T_H)
+// Nb tuiles d'entrée par ligne de buffer
+# define T_IN_L_NB p_WIDTH / B_W
+// Nb tuiles de sortie par ligne
+# define T_OUT_L_NB p_WIDTH / T_W
+
+#define tmpl(x) template<typename wb_param> x VideoCalc<wb_param>
+
+using namespace sc_core;
+using namespace std;
+
+//Pour l'instant on se contente de lire puis de stocker les
+//tules en ram sans effectuer aucune operation.
+
+namespace soclib { namespace caba {
+    template<typename wb_param>
+      class VideoCalc:sc_module
+    {
+      public:
+      // Ports E/S
+
+      sc_in_clk	clk;
+      sc_in<bool> reset_n;
+
+      //Wishbone
+      sc_core::sc_in<bool> p_clk;
+      sc_core::sc_in<bool> p_resetn;
+      soclib::caba::WbMaster<wb_param> p_wb_read;
+      soclib::caba::WbMaster<wb_param> p_wb_write;
+
+      //Interruptions de fin d'écriture d'une image
+      sc_out<bool> p_interrupt;
+
+      /////////////////////////////////////
+      // Constructeur
+      /////////////////////////////////////
+
+      VideoCalc (sc_core::sc_module_name insname,
+                 uint32_t * tab,
+                 const int w = 640,		//largeur d'image par défaut
+                 const int h = 480		//hauteur d'image par défaut
+        );
+
+      //////////////////////////////////////
+      // Methodes et parametres internes
+      /////////////////////////////////////
+      void get_buffer();
+      void process_tile();
+      void store_tile();
+      void process_center(int tile_nb);
+      void process_invimg(int tile_nb, int invimg_c[T_H][T_W], int invimg_l[T_H][T_W]);
+      void buffer_fill(uint32_t img_adr_in,int tile_nb);
+
+      private:
+
+      // Pour contenir la zone en buffer
+      uint8_t buffer[B_H][B_W];
+
+      // Coordonnées du pixel au centre de la zone de buffer
+      int buffer_center_c;
+      int buffer_center_l;
+
+      // paramètres de l'image
+      const uint32_t p_WIDTH ;
+      const uint32_t p_HEIGHT ;
+
+      // Pour indiquer qu'il est temps de remplir
+      // le buffer
+      //sc_signal<bool> ask_buffer;
+
+      bool ask_buffer;
+
+      // Pour indique que le buffer a été rempli
+      sc_signal<bool> buffer_rdy;
+
+      // Fifo de tuile à stocker en RAM
+      sc_fifo<unsigned char> fifo;
+
+      // variable contenant l'adresse des images
+      uint32_t img_adr_in;
+      uint32_t img_adr_out;
+
+      // booleen permettant de savoir si on doit
+      // traiter une nouvelle image
+      bool img_proc;
+
+      // Adresse de lecture et ecriture en RAM
+      uint32_t * wb_tab;
+
+      // Maître wishbone pour la lecture en RAM
+      WbMasterModule<wb_param> master0;
+
+      // Maître wishbone pour l'écriture en RAM
+      WbMasterModule<wb_param> master1;
+
+      protected:
+      SC_HAS_PROCESS(VideoCalc);
+
+    };
+  }}
+#endif //VIDEO_CALC_H
+
+
+
+
