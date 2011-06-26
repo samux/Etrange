9a0e255d070ac610b574732c0237da2c32e27f46
Florian Thorey <florian.thorey@gmail.com>
2011-06-26 19:14:42 +0200

    Doxygenisation de video_out.

--- video_in_dev/video_out/video_out.cpp
+++ video_in_dev/video_out/video_out.cpp
@@ -1,173 +1,174 @@
-/************************************************************
+/*!
+ * \file video_calc.cpp
+ * \brief This file contains the module which processes an image
+ * \author Caroline Keramsi
+ * \date 20/06/2011
  *
- *      File : video_gen.cpp
- *      Author: T. Graba
- *      Credits: A. Polti
- *      Telecom ParisTECH
+ *  Two threads :  - One that reads the pixel in the RAM and stores them in the fifo.
+ *                 - One that reads the fifo and regenerate the video stream
  *
- ************************************************************/
+ */
+
 
 #include "video_out.h"
 #define tmpl(x) template<typename wb_param> x VideoOut<wb_param>
 namespace soclib { namespace caba {
 
-	// le constructeur
-	tmpl(/**/)::VideoOut (sc_core::sc_module_name insname,
-			uint32_t * wb_tab,
-			const int w ,
-			const int h ,
-			const int lsync ,
-			const int fsync ):
-		sc_core::sc_module(insname),
-		p_WIDTH(w),
-		p_HEIGHT(h),
-		p_LINE_SYNC(lsync),
-		p_FRAME_SYNC(fsync),
-		wb_tab(wb_tab),
-		p_clk("p_clk"),
-		p_resetn("p_resetn"),
-		master0(p_clk,p_resetn, p_wb),
-		fifo(48*5)
-	{
-		// Lecture des pixels en RAM
-		SC_THREAD(read_image);
-		sensitive << p_clk.pos();
-		dont_initialize();
+    /////////////////////////////////////////////////////////////////////
+    //	Constructor
+    ////////////////////////////////////////////////////////////////////
 
-		// Génération de la sortie vidéo
-		SC_THREAD(gen_sorties);
-		sensitive << pixel_clk.pos();
-		dont_initialize();
+    tmpl(/**/)::VideoOut (sc_core::sc_module_name insname,
+                          uint32_t * wb_tab,
+                          const int w ,
+                          const int h ,
+                          const int lsync ,
+                          const int fsync ):
+               sc_core::sc_module(insname),
+               p_WIDTH(w),
+               p_HEIGHT(h),
+               p_LINE_SYNC(lsync),
+               p_FRAME_SYNC(fsync),
+               wb_tab(wb_tab),
+               p_clk("p_clk"),
+               p_resetn("p_resetn"),
+               master0(p_clk,p_resetn, p_wb),
+               fifo(48*5)
+    {
+      SC_THREAD(read_image);
+      sensitive << p_clk.pos();
+      dont_initialize();
 
-		std::cout << name()
-			<< " was created succesfully " << std::endl;
-	}
+      SC_THREAD(gen_sorties);
+      sensitive << pixel_clk.pos();
+      dont_initialize();
 
-	// Génère les sorties en lisant en continu
-	// dans la fifo
-	tmpl(void)::gen_sorties()
-	{
-		std::cout << " VOUT GEN_SORTIES: START " << std::endl;
+      std::cout << name()
+                << " was created succesfully " << std::endl;
+    }
 
-		unsigned char pixel_tmp;
+    ///////////////////////////
+    // GEN_SORTIES
+    //////////////////////////
 
-		for(;;)
-		{
+    tmpl(void)::gen_sorties()
+    {
+      std::cout << " VOUT GEN_SORTIES: START " << std::endl;
 
-			if(p_resetn == false)
-			{
-reset:
-				// On met les sorties à zero
-				pixel_out = 0;
-				line_valid = false;
-				frame_valid = false;
-				p_interrupt = 0;
-				std::cout << " VOUT GEN_SORTIES: RESET " << std::endl;
-				wait();
-			}
+      unsigned char pixel_tmp;
 
-			else
-			{
-				while (fifo.num_available() == 0)
-					wait();
+      for(;;)
+      {
 
-				for(unsigned int i=0; i<( p_HEIGHT + p_FRAME_SYNC ); i++)
-					for(unsigned int j=0; j<( p_WIDTH + p_LINE_SYNC ); j++)
-					{
-						// Si on est dans la fenêtre active, on sort le pixel courant
-						// Rappel : une trame video fait ( p_WIDTH + p_LINE_SYNC )*( p_HEIGHT + p_FRAME_SYNC ),
-						// l'image active est de p_WIDTH*p_HEIGHT
-						if((i<p_HEIGHT) && (j>p_LINE_SYNC-1))
-						{
-							if (!fifo.nb_read(pixel_tmp))
-							{
-								std::cout<< "VOUT GEN_SORTIES: FIFO VIDE" <<std::endl;
-							}
-							else
-								pixel_out = pixel_tmp;
-						}
-						else
-							pixel_out = 0xBB;
+        if(p_resetn == false)
+        {
+          reset:
 
-						// Generation de line valid
-						line_valid = (i<p_HEIGHT) && (j>p_LINE_SYNC-1);
+          pixel_out = 0;
+          line_valid = false;
+          frame_valid = false;
+          p_interrupt = 0;
+          std::cout << " VOUT GEN_SORTIES: RESET " << std::endl;
+          wait();
+        }
 
-						// Generation de frame valid
-						frame_valid = (i<p_HEIGHT);
+        else
+        {
+          while (fifo.num_available() == 0)
+            wait();
 
-						// Puis on attend le prochain coup d'horloge, ou un reset
-						wait();
+          for(unsigned int i=0; i<( p_HEIGHT + p_FRAME_SYNC ); i++)
+            for(unsigned int j=0; j<( p_WIDTH + p_LINE_SYNC ); j++)
+            {
 
-						// Si on est reveillé par un reset, on revient à la case départ
-						// (les goto sont dans ce genre de cas bien pratique...)
-						if(p_resetn == false)
-							goto reset;
-					}
+              // If we are in the active windows, the current pixel is processed.
+              if((i<p_HEIGHT) && (j>p_LINE_SYNC-1))
+              {
+                if (!fifo.nb_read(pixel_tmp))
+                {
+                  std::cout<< "VOUT GEN_SORTIES: FIFO VIDE" <<std::endl;
+                }
+                else
+                  pixel_out = pixel_tmp;
+              }
+              else
+                pixel_out = 0xBB;
 
-				p_interrupt = 1;
-                                wb_tab[3] = 0;
-                                std::cout << " VOUT GEN_SORTIES: INTERRUPTION SENT " << std::endl;
-				wait();
-				wait();
-				wait();
-				p_interrupt = 0;
-			}
-		}
-	}
+              // Generation of line valid
+              line_valid = (i<p_HEIGHT) && (j>p_LINE_SYNC-1);
 
-	// Lecture de l'image en RAM à partir de l'adresse
-	// spécifiée par le processeur par whisbone (contenu
-	// dans wb_tab) puis écriture dans la fifo
-	tmpl(void)::VideoOut::read_image()
-	{
-		//Les pixels de type uint8_t sont groupés
-		//par 4 dans la mémoire.
-		uint32_t im_addr;
-		uint32_t buffer[VOUT_PACK];
+              // Generation of frame vad
+              frame_valid = (i<p_HEIGHT);
 
-		std::cout << " VOUT READ_IMAGE: START " << std::endl;
+              // Then we wait for the next clock rising edge or a reset
+              wait();
 
-		for (;;) {
+              // If we are woke up by a reset, we restart from the beginning
+              if(p_resetn == false)
+                goto reset;
+            }
 
-			if (p_resetn == false)
-			{
-                          im_addr = 0;
-                          wb_tab[3] = 0;
-                          wb_tab[2] = 0;
-                          while (fifo.read());
-			}
+          p_interrupt = 1;
+          wb_tab[3] = 0;
+          std::cout << " VOUT GEN_SORTIES: INTERRUPTION SENT " << std::endl;
+          wait();
+          wait();
+          wait();
+          p_interrupt = 0;
+        }
+      }
+    }
+    // Reading of the frame in the RAM from the address given
+    // by the processor through the wishbone bus
+    tmpl(void)::VideoOut::read_image()
+    {
+      // Pixels are packed 4 by 4 because
+      // it's the lenght of the bursts that will
+      // be read from the ram
+      uint32_t im_addr;
+      uint32_t buffer[VOUT_PACK];
 
-			//Nouvelle image, on met à jour l'adresse de l'image
-			//en ram
-			while (!wb_tab[3])
-                          wait();
+      std::cout << " VOUT READ_IMAGE: START " << std::endl;
 
-			im_addr = wb_tab[2];
-			wb_tab[3] = 0;
+      for (;;) {
 
-			std::cout << " VOUT READ_IMAGE: NOUVELLE ADRESSE " << wb_tab[2] << std::endl;
+        if (p_resetn == false)
+        {
+          im_addr = 0;
+          wb_tab[3] = 0;
+          wb_tab[2] = 0;
+          while (fifo.read());
+        }
 
-			for (unsigned int i = 0; i < (p_HEIGHT * p_WIDTH) / (VOUT_PACK * 4); i++)
-			{
-				//std::cout << " VOUT READ_IMAGE: LECTURE DE" << VOUT_PACK << "mots en " << im_addr+i*VOUT_PACK << std::endl;
+        // If we got a new frame, we update the
+        // address of the frame in ram
+        while (!wb_tab[3])
+          wait();
 
-				master0.wb_read_blk(im_addr + 4 * i * VOUT_PACK,
-						VOUT_PACK,
-						buffer);
+        im_addr = wb_tab[2];
+        wb_tab[3] = 0;
 
-				for (int j = 0; j < VOUT_PACK; j++)
-				{
-					for (int k = 3; k>=0; k--)
-					{
-						fifo.write(buffer[j] >> 8 * k);
-						buffer[j] = buffer[j] - ((buffer[j] >> 8 * k) << 8 * k);
-					}
-				}
+        std::cout << " VOUT READ_IMAGE: NOUVELLE ADRESSE " << wb_tab[2] << std::endl;
 
-			}
-                        wb_tab[3] = 0;
-		}
-	}
+        for (unsigned int i = 0; i < (p_HEIGHT * p_WIDTH) / (VOUT_PACK * 4); i++)
+        {
+          master0.wb_read_blk(im_addr + 4 * i * VOUT_PACK,
+                              VOUT_PACK,
+                              buffer);
 
-}}
+          for (int j = 0; j < VOUT_PACK; j++)
+          {
+            for (int k = 3; k>=0; k--)
+            {
+              fifo.write(buffer[j] >> 8 * k);
+              buffer[j] = buffer[j] - ((buffer[j] >> 8 * k) << 8 * k);
+            }
+          }
+
+        }
+        wb_tab[3] = 0;
+      }
+    }
+
+  }}
 
--- video_in_dev/video_out/video_out.h
+++ video_in_dev/video_out/video_out.h
@@ -1,17 +1,18 @@
-/*************************************************************
+/*!
+ * \file video_calc.cpp
+ * \brief This file contains the module which processes an image
+ * \author Caroline Keramsi
+ * \date 20/06/2011
  *
- *      File : video_gen.cpp
- *      Author: T. Graba
- *      Credits: A. Polti
- *      Telecom ParisTECH
+ *  Two threads :  - One that reads the pixel in the RAM and stores them in the fifo.
+ *                 - One that reads the fifo and regenerate the video stream
  *
- ************************************************************/
+ */
 
 
 #ifndef VIDEO_OUT_H
 #define VIDEO_OUT_H
 
-#define VOUT_OFFSET 1
 #define VOUT_PACK 32
 
 #include <systemc>
@@ -29,46 +30,77 @@ namespace soclib { namespace caba {
 
       public:
 
-      sc_core::sc_in<bool>              p_clk;
-      sc_core::sc_in<bool>              p_resetn;
-      sc_core::sc_out<bool>             p_interrupt;
+      sc_core::sc_in<bool>              p_clk;            /*!< System clock (default = 100Mhz) */
+      sc_core::sc_in<bool>              p_resetn;         /*!< Reset signal */
+      sc_core::sc_out<bool>             p_interrupt;      /*!< to indicate that a frame has been processed */
 
-      sc_core::sc_in<bool>              pixel_clk;
-      sc_core::sc_out<unsigned char>    pixel_out;
-      sc_core::sc_out<bool>             line_valid;
-      sc_core::sc_out<bool>             frame_valid;
+      sc_core::sc_in<bool>              pixel_clk;        /*!< Pixel clock (default = 25Mhz) */
+      sc_core::sc_out<unsigned char>    pixel_out;        /*!< The outcoming pixel */
+      sc_core::sc_out<bool>             line_valid;       /*!< Line valid to respect the format of the video stream */
+      sc_core::sc_out<bool>             frame_valid;      /*!< Frame valid to respect the format of the video stream */
 
-      WbMaster<wb_param>                p_wb;
+      WbMaster<wb_param>                p_wb;             /*!<  */
 
-      ////////////////////////////////////////////////////
-      //	constructor
-      ////////////////////////////////////////////////////
+      /*!
+       * \brief Constructor
+       *
+       * VideoOut constructor
+       *
+       * \param tab: the link with the processor
+       * \param w: image width (640 by default)
+       * \param h: image height (480 by default)
+       * \param lsync: synchronisation with the lines
+       * \param fsync: synchronisation with the frames
+       * */
+
       VideoOut(sc_module_name insname,
                uint32_t * wb_tab,
-               const int w = 640,          // largeur d'image par defaut
-               const int h = 480,          // hauteur par defaut
-               const int lsync = 160,      // synchro ligne par defaut
-               const int fsync = 40        // synchro trame par defaut
+               const int w = 640,
+               const int h = 480,
+               const int lsync = 160,
+               const int fsync = 40
         );
 
-      ////////////////////////////////////////////////////
-      //	methods and structural parameters
-      ////////////////////////////////////////////////////
+      /*!
+       * \brief To regenerate the video stream
+       *
+       * Wait for the fifo the contains a minimal amount
+       * of pixels, then regenerate the video stream by
+       * taking the pixels from this fifo
+       *
+       */
+
       void                gen_sorties();
+
+      /*!
+       * \brief To read frams in the ram and fill the fifo
+       *
+       * Wait for an address sent by the processor to be valid.
+       * As soon as an address is valid, this process starts reading
+       * the ram and filling the fifo
+       *
+       */
       void                read_image();
 
       private:
 
-      // paramètres de l'image
+
       const uint32_t  p_WIDTH      ;
+      /*!< Image width */
       const uint32_t  p_HEIGHT     ;
+      /*!< Image height */
       const uint32_t  p_LINE_SYNC  ;
+      /*!< Line synchronisation */
       const uint32_t  p_FRAME_SYNC ;
+      /*!< Frame synchronisation */
 
       sc_fifo<uint8_t>   fifo;
+      /*!< To communicate between the two threads */
       uint32_t * wb_tab;
+      /*!< To communicate with the processor */
 
       WbMasterModule<wb_param> master0;
+      /*!< wishbone master to read in RAM */
 
       protected:
       SC_HAS_PROCESS(VideoOut);
